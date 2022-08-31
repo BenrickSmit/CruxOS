a42d16f6464884291f891ab509d787bcc0314418
Metatronic <55592806+BenrickSmit@users.noreply.github.com>
2022-08-31 15:22:32 +0900

    Create CruxOS_Output.h
    Initial Commit

---
+++ includes/CruxOS_Output.h
@@ -0,0 +1,10 @@
+/** This is the CruxOS Output header
+ ** @Author: Benrick G. Smit
+ ** @Date: 2022/08/31
+ ** @Edited: 2022/08/31
+ ** @Description: Output System for Crux OS
+**/
+
+#pragma once
+
+#include <cstdlib>
