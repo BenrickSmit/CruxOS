e9304099363602f8d924feb95a132304055d9814
Metatronic <55592806+BenrickSmit@users.noreply.github.com>
2022-08-31 15:21:42 +0900

    Create CruxOS_Its.h
    Initial Commit

---
+++ includes/CruxOS_Its.h
@@ -0,0 +1,10 @@
+/** This is the CruxOS Action Interrupt Request header
+ ** @Author: Benrick G. Smit
+ ** @Date: 2022/08/31
+ ** @Edited: 2022/08/31
+ ** @Description: Action Interrupt Request System for Crux OS
+**/
+
+#pragma once
+
+#include <cstdlib>
