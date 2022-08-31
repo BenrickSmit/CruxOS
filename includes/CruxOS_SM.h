b045f255d4ca7f7d2dceaea1408c9dcc54d6ff0c
Metatronic <55592806+BenrickSmit@users.noreply.github.com>
2022-08-31 15:19:13 +0900

    Create CruxOS_SM.h
    Initial Commit

---
+++ includes/CruxOS_SM.h
@@ -0,0 +1,10 @@
+/** This is the CruxOS Storage Management header
+ ** @Author: Benrick G. Smit
+ ** @Date: 2022/08/31
+ ** @Edited: 2022/08/31
+ ** @Description: Storage Management System for Crux OS
+**/
+
+#pragma once
+
+#include <cstdlib>
